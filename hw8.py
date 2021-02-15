382b392be51f0f31b421f32e7cc1d56f6374f9ca
kaminskyi <vladimir_kaminskiy@ukr.net>
2021-02-15 20:11:28 +0200

    done

--- hw8.py
+++ hw8.py
@@ -54,17 +54,25 @@ def random_string(min_limit, max_limit):
 print(random_string(10, 50))
 
 
-# 3
+# 3. Написать функцию (или последовательность нескольких функций), которые преобразуют случайную строку,
+# полученную в п 2 по следующим правилам:
+# В строке должны присутствовать большие и маленькие буквы английского алфавита, цифры, пробелы,
+# знаки препинания, символ перехода на новую строку (\n).
+# Строка должна выглядеть как текст. Слова отделяться друг от друга пробелами.
+# Под словом будем понимать набор случайных букв от одной до 10.
+# Большие буквы только в начале слов. Цифры не должны быть частями слов, а стоять отдельно.
+# Знаки препинания всегда идут в конце слова.
 
 
-def transform_text(text):
-    index = 0
+def modify_text(text):
+    i = 0
     text = list(text)
-    while index + 10 < len(text):
-        punctuation = [", ", " ", "\n", " " + str(randint(1, 50)) + " "]
-        space_point = randint(2, 10)
-        index += space_point
-        text[index] = choice(punctuation)
+    while i + 10 < len(text):
+        numb =" " + str(randint(1, 50)) + " "
+        symb = [", ", " ", "\n",numb ]
+        space = randint(1, 10)
+        i += space
+        text[i] = choice(symb)
     text.append(".")
     text = "".join(text)
     text = text.title()
@@ -72,14 +80,16 @@ def transform_text(text):
 
 
 
-def add_lower_case(text):
+def lowercase(text):
     i = 0
     text = list(text)
     while i + 3 < len(text):
-        upper_point = randint(1,3)
-        i += upper_point
+        uppercase = randint(1,3)
+        i += uppercase
         text[i] = text[i].lower()
     return "".join(text)
 
 
-print(add_lower_case(transform_text(random_string(100,500))))
+print(lowercase(modify_text(random_string(150,400))))
+print(lowercase('AAAAAAAAA'))
+
